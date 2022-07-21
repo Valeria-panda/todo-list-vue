c4379466e2eb8bb5cf75fdd95e4d4750e2186b16
Valeria Zolotaryova <panda.lera@mail.ru>
2022-07-21 14:12:13 +0500

    add eventlisteners on button next and prev

--- src/components/StandartPagination.vue
+++ src/components/StandartPagination.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="catalog__pagination pagination">
         <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow pagination__link--disabled"
+            <a @click.prevent="prevButton()" class="pagination__link pagination__link--arrow pagination__link--disabled"
                 aria-label="Предыдущая страница">
                 <svg width="8" height="14" fill="currentColor">
                     <use xlink:href="#icon-arrow-left"></use>
@@ -15,7 +15,8 @@
             </a>
         </li>
         <li class="pagination__item">
-            <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница">
+            <a @click.prevent="nextButton()" class="pagination__link pagination__link--arrow" href="#"
+                aria-label="Следующая страница">
                 <svg width="8" height="14" fill="currentColor">
                     <use xlink:href="#icon-arrow-right"></use>
                 </svg>
@@ -49,10 +50,73 @@ export default {
     methods: {
         paginate(page) {
             this.$emit('update:page', page);
+        },
+        nextButton() {
+            this.$emit('update:page', this.page + 1);
+        },
+        prevButton() {
+            this.$emit('update:page', this.page - 1);
         }
     }
 }
 </script>
 <style>
+.catalog__pagination {
+    margin-top: auto
+}
+
+.pagination {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center
+}
+
+.pagination__item:first-child {
+    margin-right: 25px
+}
+
+.pagination__item:last-child {
+    margin-left: 25px
+}
+
+.pagination__link {
+    display: block;
+    padding: 13px 3px;
+    height: 40px;
+    min-width: 40px;
+    font-size: 16px;
+    line-height: 1;
+    color: #222;
+    text-align: center;
+    -webkit-transition: all .2s ease;
+    transition: all .2s ease
+}
+
+.pagination__link[href]:not(:disabled):focus,
+.pagination__link[href]:not(:disabled):hover {
+    opacity: .6
+}
+
+.pagination__link--current {
+    font-weight: 700
+}
+
+.pagination__link--arrow {
+    border: 1px solid #e2e2e2
+}
+
+.pagination__link--disabled {
+    opacity: .6;
+    cursor: not-allowed
+}
 </style>
  
